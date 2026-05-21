2910d1d49ca3ae311d58377b7f89c2bf5ed8c520
cherniavskyi <cherniavskyi.utd@gmail.com>
2026-05-21 22:09:42 +0200

    Lebenslauf build fix

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,7 +1,7 @@
 import styles from './intro.module.scss';
 
 type Props = {
-  setActiveSection: React.Dispatch<React.SetStateAction<number>>;
+  setActiveSection?: React.Dispatch<React.SetStateAction<number>>;
 };
 
 const Intro = ({ setActiveSection }: Props) => {
@@ -10,7 +10,7 @@ const Intro = ({ setActiveSection }: Props) => {
       <div className={styles.intro__banner}>
         <h1>Oleh<br />Cherniavskyi</h1> 
         <h2>Frontend Entwickler</h2>
-        <button onClick={() => setActiveSection(4)}>Kontakt
+        <button onClick={() => setActiveSection && setActiveSection(4)}>Kontakt
           <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 150 118" xmlSpace="preserve">
             <g transform="translate(0.000000,118.000000) scale(0.100000,-0.100000)">
               <path d="M870,1167c-34-17-55-57-46-90c3-15,81-100,194-211l187-185l-565-1c-431,0-571-3-590-13c-55-28-64-94-18-137c21-20,33-20,597-20h575l-192-193C800,103,794,94,849,39c20-20,39-29,61-29c28,0,63,30,298,262c147,144,272,271,279,282c30,51,23,60-219,304C947,1180,926,1196,870,1167z"/>
